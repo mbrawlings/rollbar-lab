7f153049c8481e25216ef8fb2a7e4f68fb15524d
Matthew Rawlings <mbrawlings11@gmail.com>
2022-09-01 14:53:54 -0600

    initial commit

---
+++ server/server.js
@@ -0,0 +1,25 @@
+const express = require('express')
+const cors = require('cors')
+const path = require('path')
+require('dotenv').config()
+
+const app = express()
+
+const { ROLLBARTOKEN, PORT } = process.env
+
+app.use(express.json())
+app.use(cors())
+
+var Rollbar = require('rollbar')
+var rollbar = new Rollbar({
+  accessToken: ROLLBARTOKEN,
+  captureUncaught: true,
+  captureUnhandledRejections: true,
+})
+
+// record a generic message and send it to Rollbar
+rollbar.log('Hello world!')
+
+app.listen(PORT, () => {
+    `Server listening on port ${PORT}`
+})
